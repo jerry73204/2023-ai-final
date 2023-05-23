0ec37f98e3a907dd47befeff8ae4712dffc7b752
aeon <jerry73204@gmail.com>
2023-05-23 14:25:31 +0800

    Tidy code

--- jigsaw-diffusion/jigsaw_diffusion/jigsaw_datasets.py
+++ jigsaw-diffusion/jigsaw_diffusion/jigsaw_datasets.py
@@ -4,35 +4,39 @@ import torch
 import cv2 as cv
 import csv
 import os
-import numpy
 from torch.utils.data import Dataset
+
+
 class JigsawDataset(Dataset):
-    def __init__(self, data_dir,batch_size,piece_size):
-        self.x, self.y = self.load_data(data_dir,batch_size, piece_size)
+    def __init__(self, data_dir, batch_size, piece_size):
+        self.x, self.y = self.load_data(data_dir, batch_size, piece_size)
         self.n_samples = len(self.x)
+
     def __getitem__(self, index):
         return self.x[index], self.y[index]
+
     def __len__(self):
         return self.n_samples
 
-    def load_data(self,data_dir: Path, batch_size, piece_size: int):
-            # Load manifest.json5
+    def load_data(self, data_dir: Path, batch_size, piece_size: int):
+        # Load manifest.json5
         manifest_path = os.path.join(data_dir, "manifest.json5")
         with open(manifest_path, "r") as fp:
             manifest = json5.load(fp)
         samples = list(
-            self.load_puzzle_dir(data_dir, puzzle_info) for puzzle_info in manifest["puzzles"]
+            self.load_puzzle_dir(data_dir, puzzle_info)
+            for puzzle_info in manifest["puzzles"]
         )
         # print(len(samples))
         # print(samples[0][0].shape, samples[0][1].shape)
         # print(samples[1][0].shape, samples[1][1].shape)
         images, labels = zip(*samples)
-        
+
         # print(images[0].shape, images[1].shape)
         # print(labels[0].shape, labels[1].shape)
         return images, labels
 
-    def load_puzzle_dir(self,data_dir: Path, puzzle_info):
+    def load_puzzle_dir(self, data_dir: Path, puzzle_info):
         puzzle_name = puzzle_info["name"]
         n_pieces = puzzle_info["n_pieces"]
         n_pieces = 25
@@ -58,9 +62,9 @@ class JigsawDataset(Dataset):
             rx = 0
             for r in rows:
                 rx += 1
-                if (rx > n_pieces):
+                if rx > n_pieces:
                     break
-                nr = [float(sr) for sr in r[0].split('\t')]
+                nr = [float(sr) for sr in r[0].split("\t")]
                 label.append(nr)
             assert len(label) == n_pieces
             label = torch.FloatTensor(label)
