c741f0538deef496003886bf0627b97f5d5e66d1
aeon <jerry73204@gmail.com>
2023-05-24 22:53:51 +0800

    Tidy train.py

--- jigsaw-diffusion/jigsaw_diffusion/jigsaw_train_util.py
+++ jigsaw-diffusion/jigsaw_diffusion/jigsaw_train_util.py
@@ -154,7 +154,7 @@ class TrainLoop:
         while (
             not self.lr_anneal_steps
             or self.step + self.resume_step < self.lr_anneal_steps
-        ):  
+        ):
             batch, cond = next(self.data)
             self.run_step(batch, cond)
             if self.step % self.log_interval == 0:
