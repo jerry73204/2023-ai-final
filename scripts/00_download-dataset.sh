da6bc29dc0b384adf2db771484bda080ff670486
aeon <jerry73204@gmail.com>
2023-05-24 15:40:16 +0800

    Create a script to generate a puzzle dataset.

--- scripts/00_download-dataset.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
-cd "$script_dir"
-
-mkdir -p ../dataset/orig
-kaggle datasets download etaifour/jigsawpuzzle --unzip --path ../dataset/orig
---
+++ scripts/00_generate-dataset.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -e
+
+n_puzzles=100
+
+script_dir=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+
+cd "$script_dir/.."
+mkdir dataset/puzzles
+
+cd "$script_dir/../puzzle-generator"
+
+for (( pid=0; pid<$n_puzzles; pid++ )); do
+    echo "poetry run main -- ../dataset/puzzles/$(printf 'p%04d' $pid)"
+done | parallel
+
+{
+    echo '{'
+    echo '  "puzzles": ['
+
+    for (( pid=0; pid<$n_puzzles; pid++ )); do
+        name=$(printf 'p%04d' $pid)
+
+        echo '    {'
+        echo "      \"name\": \"${name}\","
+        echo "      \"n_pieces\": 25,"
+        echo '    },'
+    done
+
+    echo '  ]'
+    echo '}'
+} > "$script_dir/../dataset/puzzles/manifest.json5"
