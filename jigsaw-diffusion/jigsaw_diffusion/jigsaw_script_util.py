4a846bfe5f11602d9447569db7b81f21db312e25
aeon <jerry73204@gmail.com>
2023-05-30 21:22:43 +0800

    Fix incorrect position de/normalization

--- jigsaw-diffusion/jigsaw_diffusion/jigsaw_script_util.py
+++ jigsaw-diffusion/jigsaw_diffusion/jigsaw_script_util.py
@@ -229,25 +229,25 @@ def normalize_piece_images(images: torch.FloatTensor) -> torch.FloatTensor:
 
 
 def denormalize_piece_images(images: torch.FloatTensor) -> torch.FloatTensor:
-    return ((images + 1) / 2 * 255)
+    return (images + 1) / 2 * 255
 
 
 def normalize_piece_positions(positions: torch.FloatTensor, puzzle_size: int):
     # Normalize x, y to [-1, 1]
 
-    positions[:2] = positions[:2] / puzzle_size * 2 - 1
+    positions[:, :2] = positions[:, :2] / puzzle_size * 2 - 1
 
     # Normalize rotation to [-1, 1]
-    positions[2] = positions[2] / math.pi - 1
+    positions[:, 2] = positions[:, 2] / math.pi - 1
 
     return positions
 
 
 def denormalize_piece_positions(positions: torch.FloatTensor, puzzle_size: int):
     # Denormalize x, y to [0, puzzle_size]
-    positions[:2] = (positions[:2] + 1) / 2 * puzzle_size
+    positions[:, :2] = (positions[:, :2] + 1) / 2 * puzzle_size
 
     # Denormalize rotation to [0, 2π]
-    positions[2] = (positions[2] + 1) * math.pi
+    positions[:, 2] = (positions[:, 2] + 1) * math.pi
 
     return positions
